5f53ab86136045cc64222ed151a56cb2296aa83a
Mahesh Perera <165042259+maheshkmp@users.noreply.github.com>
2024-08-25 17:56:42 +0530

    first commit

---
+++ encapsulation/encapsulation.py
@@ -0,0 +1,20 @@
+class Car:
+    def __init__(self) -> None:
+        self.__speed = 0
+
+    def accelaration(self):
+        self.__speed = self.__speed + 10
+
+    def apply_break(self):
+        self.__speed = self.__speed - 5
+
+    def get_speed(self):
+        return self.__speed
+    
+
+car = Car()
+car.accelaration()
+car.accelaration()
+car.accelaration()
+car.apply_break()
+print(car.get_speed)
